7e119ec1448e4f8f831d499c19b78393eb314c74
dhairyakhale <dhairyakhale@gmail.com>
2023-02-11 17:23:16 +0530

    Schema fields

--- contracts/LandInit.sol
+++ contracts/LandInit.sol
@@ -3,5 +3,16 @@
 pragma solidity >=0.6.0 <=0.9.0;
 
 contract LandInit {
-    string public landID;
+    struct landDetails {
+        uint256[] landID; //Slashes mean moving ahead in index
+        string state;
+        string district;
+        string subdivision;
+        uint256 val_INR; //Round figure always
+        uint256 area_HA; //Multiplier is 1000, so 3 decimal places allowed
+    }
+
+    struct user_details {
+        string[] full_name;
+    }
 }
