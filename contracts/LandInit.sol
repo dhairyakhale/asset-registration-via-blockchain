52a2aa51db98d2cde225cd615761766d19b56318
dhairyakhale <dhairyakhale@gmail.com>
2023-02-15 15:54:26 +0530

    function skeleton added

--- contracts/LandInit.sol
+++ contracts/LandInit.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <=0.9.0;
+pragma experimental ABIEncoderV2;
 
 contract LandInit {
-    struct landDetails {
+    struct land_details {
         uint256[] landID; //Slashes mean moving ahead in index
         string state;
         string district;
@@ -18,5 +19,18 @@ contract LandInit {
         string pan_no;
     }
 
-    function registerUser() 
+    function registerUser(user_details memory user) public {
+        // Check if hash exists from before
+        // Auth for all user details
+        // Check eligibility to make land txn
+    }
+
+    function registerLand() public {
+        // Check if land ID exists or not
+        // Check and verify if owner exists in system
+        // save land in database
+        // generate hash and list on blockchain
+    }
+
+    function transactLand() public {}
 }
