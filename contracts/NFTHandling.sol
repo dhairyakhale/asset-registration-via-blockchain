62586a62193d85ca800a3cb3117ff8ac84aafe36
dhairyakhale <dhairyakhale@gmail.com>
2023-04-27 16:45:27 +0530

    Not sure did what this time

--- contracts/NFTHandling.sol
+++ contracts/NFTHandling.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";   //ERROR
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";     //ERROR
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
