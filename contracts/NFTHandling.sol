5dc0f1201b50dd9df8cfeb0efcace09bebf26f28
dhairyakhale <dhairyakhale@gmail.com>
2023-04-27 18:31:34 +0530

    idk what I did

--- contracts/NFTHandling.sol
+++ contracts/NFTHandling.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";   //ERROR
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; //ERROR
 // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";     //ERROR
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
