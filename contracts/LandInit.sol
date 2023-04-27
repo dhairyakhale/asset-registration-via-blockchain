33329e23903bde9f80a50035323094007e328b02
dhairyakhale <dhairyakhale@gmail.com>
2023-04-27 17:25:17 +0530

    registerLand() in progress

--- contracts/LandInit.sol
+++ contracts/LandInit.sol
@@ -16,6 +16,7 @@ contract LandInit {
 
     struct user_details {
         string username;
+        address wallet_addr;
         string[] full_name;
         uint256 dob;
         string aadhar_no;
@@ -29,14 +30,16 @@ contract LandInit {
     }
 
     string[] usernames;
-    mapping(string => user_details) user_list;
-    mapping(string => land_details) land_info; //Indirect mapping from land to users (ID->land)
-    mapping(string => string[]) owner_info; //ID->username
+    string[] land_id_list;
+    mapping(string => user_details) user_list; //username->userData
+    mapping(string => land_details) land_info; //LandID->landData
+    mapping(string => string) owner_info; //LandID->username
 
-    // mapping(address => string) acc_to_uname;
+    mapping(address => string) addr_to_uname;
 
     function createUserStruct(
         string memory _username,
+        address _wallet_addr,
         string[] memory _full_name,
         uint256 _dob,
         string memory _aadhar_no,
@@ -48,6 +51,7 @@ contract LandInit {
         // check if user is a valid person (above agelimit)
 
         user.username = _username;
+        user.wallet_addr = _wallet_addr;
         user.full_name = _full_name;
         user.aadhar_no = _aadhar_no;
         user.pan_no = _pan_no;
@@ -61,6 +65,20 @@ contract LandInit {
             if (
                 keccak256(abi.encode(_username)) ==
                 keccak256(abi.encode(usernames[i]))
+            ) {
+                //found
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function verifyLand(string memory _land_id) public returns (bool) {
+        for (uint256 i = 0; i < land_id_list.length; i++) {
+            if (
+                keccak256(abi.encode(_land_id)) ==
+                keccak256(abi.encode(land_id_list[i]))
             ) {
                 //found
                 return true;
@@ -85,10 +103,12 @@ contract LandInit {
                 return;
             }
         }
+
         // if it does not:
         // save user in the database
         user_details memory newuser = createUserStruct(
             _username,
+            curruser,
             _full_name,
             _dob,
             _aadhar_no,
@@ -99,18 +119,25 @@ contract LandInit {
         user_list[_username] = newuser;
     }
 
-    function registerLand(string memory _username) public {
-        // Check if land ID exists or not
-        // Check and verify if owner exists in system
-        if (!verifyOwner(_username)) return;
-        // save land in database
-
+    // do this properly
+    function registerLand(
+        string _owner,
+        string _land_id,
+        string _state,
+        string _district,
+        uint256 _val_inr,
+        uint256 _area_ha
+    ) public {
+        if (_owner != addr_to_uname[curruser]) {
+            // Check and verify if owner exists in system
+            if (!verifyOwner(_owner)) return;
+        }
         // mint nft by details
     }
 
     function transactLand(
         string memory _buyer,
         string memory _seller,
-        land_details memory _land
+        string memory _land_id
     ) public {}
 }
--- contracts/NFTHandling.sol
+++ contracts/NFTHandling.sol
@@ -12,7 +12,7 @@ contract MyToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyToken", "MTK") {}
+    constructor() ERC721("AssetToken", "AST") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
