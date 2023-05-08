3966ed666e864b9d0f627226f5f568a3f51f7071
dhairyakhale <dhairyakhale@gmail.com>
2023-05-08 12:26:37 +0530

    more code

--- scripts/driver_code.py
+++ scripts/driver_code.py
@@ -2,26 +2,36 @@ from brownie import accounts, config, LandInit, LandToken, network
 from scripts.json_handler import create_txn_json, create_nft_json
 
 
-pri_key_seller = accounts[1]
-pri_key_buyer = accounts[2]
-
-
-# def get_account():
-#     if network.show_active() == "development":
-#         return accounts[1]
-#     else:
-#         return accounts.add(config["wallets"]["from_key"])
+pvt_key_seller = accounts[1]
+pvt_key_buyer = accounts[2]
 
 
 def register_user():
-    print(len(LandInit))
-    # land = LandInit[-1]
-    # uname = input("Enter username: ")
-    # if land.verifyOwner(uname) == False:
-    #     print("User already exists!")
-    #     return
-    # else:
-    #     print("NICE")
+    land_init = LandInit[-1]
+
+    _uname = input("Enter username: ")
+
+    if land_init.verifyOwner(_uname) == True:
+        print("User already exists!")
+        return
+    else:
+        _wallet_addr = pvt_key_seller
+        _full_name = input("Enter full name: ")
+        _aadhar_no = input("Enter aadhar number: ")
+        _pan_no = input("Enter PAN number: ")
+
+        land_init.registerUser(
+            _uname,
+            _wallet_addr,
+            _full_name,
+            _aadhar_no,
+            _pan_no,
+            {"from": pvt_key_seller},
+        )
+
+
+def register_land(uname):
+    land_id = input("Enter Land ID: ")
 
 
 def main():
